25b620f6ec7edfa95f91a5005a9066ce1457bf5e
PrzemekS <przemeksekula@gmail.com>
2023-04-09 21:58:09 +0200

    feat: Q-Learning added

---
+++ QLearning/agents.py
@@ -0,0 +1,124 @@
+import numpy as np
+
+class SarsaAgent:
+    def __init__(self, env, alpha, gamma, epsilon):
+        """ Initialize the agent.
+        Args:
+            env (gym.Env): environment to learn from.
+            alpha (float): learning rate.
+            gamma (float): discount factor.
+            epsilon (float): epsilon-greedy policy parameter.
+        """
+        
+        self.env = env
+        self.alpha = alpha
+        self.gamma = gamma
+        self.epsilon = epsilon
+        
+        # Sets the Q_est to a matrix of zeros with the shape of (nS, nA)
+        self.Q_est = np.array([np.zeros(env.action_space.n) for _ in range(env.observation_space.n)])
+        
+        
+    def get_action(self, state):
+        """
+        Get an action from Q_est using epsilon-greedy policy. Action is
+        optimal with the probability of (1 - epsilon) + epsilon / nA.
+        Otherwise, action is selected uniformly at random.
+        Args:
+            state (int): current state
+        Returns:
+            action (int): action to take.
+        """
+        # TODO: Implement the get_action function for SARSA agent.
+        # YOUR CODE HERE
+        pass
+        
+    def update(self, state, action, reward, next_state, next_action):
+        """ Updates the Q_est using the SARSA update rule.
+        Q(s, a) <- Q(s, a) + alpha * (reward + gamma * Q(s', a') - Q(s, a))
+        Args:
+            state (int): state at time t
+            action (int): action at time t
+            reward (int, float): received reward
+            next_state (int): state at time t+1
+            next_action (int): action at time t+1
+        """
+        # TODO: Implement the update function for SARSA agent.
+        # YOUR CODE HERE
+        pass
+    
+    def save(self, path = './SARSA_Q_est.npy'):
+        """ Saves the Q_est to a file.
+        Args:
+            path (str, optional): Path to the file. Defaults to './SARSA_Q_est.npy'.
+        """
+        np.save(path, self.Q_est)
+        
+    def load(self, path = './SARSA_Q_est.npy'):
+        """ Loads the Q_est from a file.
+        Args:
+            path (str, optional): Path to the file. Defaults to './SARSA_Q_est.npy'.
+        """
+        self.Q_est = np.load(path)
+
+
+class QLearningAgent:
+    def __init__(self, env, alpha, gamma, epsilon):
+        """ Initialize the agent.
+        Args:
+            env (gym.Env): environment to learn from.
+            alpha (float): learning rate.
+            gamma (float): discount factor.
+            epsilon (float): epsilon-greedy policy parameter.
+        """
+        
+        self.env = env
+        self.alpha = alpha
+        self.gamma = gamma
+        self.epsilon = epsilon
+        
+        # Sets the Q_est to a matrix of zeros with the shape of (nS, nA)
+        self.Q_est = np.array([np.zeros(env.action_space.n) for _ in range(env.observation_space.n)])
+        
+        
+    def get_action(self, state):
+        """
+        Get an action from Q_est using epsilon-greedy policy. Action is
+        optimal with the probability of (1 - epsilon) + epsilon / nA.
+        Otherwise, action is selected uniformly at random.
+        Args:
+            state (int): current state
+        Returns:
+            action (int): action to take.
+        """
+        # TODO: Implement the get_action function for QLearning agent.
+        # YOUR CODE HERE
+        pass
+        
+    def update(self, state, action, reward, next_state):
+        """ Updates the Q_est using the QLearning update rule.
+        Q(s, a) <- Q(s, a) + alpha * (reward + gamma * max_a' Q(s', a') - Q(s, a))
+        Args:
+            state (int): state at time t
+            action (int): action at time t
+            reward (int, float): received reward
+            next_state (int): state at time t+1
+        """
+        # TODO: Implement the update function for QLearning agent.
+        # YOUR CODE HERE
+        pass
+    
+    def save(self, path = './QLearning_Q_est.npy'):
+        """Saves the Q_est to a file.
+        Args:
+            path (str, optional): Path to the file. Defaults to './QLearning_Q_est.npy'.
+        """
+        np.save(path, self.Q_est)
+        
+    def load(self, path = './QLearning_Q_est.npy'):
+        """ Loads the Q_est from a file.
+        Args:
+            path (str, optional): Path to the file. Defaults to './QLearning_Q_est.npy'.
+        """
+        print ('PATH:', path)
+        self.Q_est = np.load(path)
---
+++ QLearning/render.py
@@ -0,0 +1,80 @@
+import argparse
+from agents import SarsaAgent, QLearningAgent
+
+import gym
+
+parser = argparse.ArgumentParser(description='Runs TD(0) on Walking5-v0.')
+
+parser.add_argument('-e', '--env',
+                    type=str,
+                    default='CliffWalking-v0',
+                    choices = ['CliffWalking-v0', 'Taxi-v3'],
+                    help='Environment. One of CliffWalking-v0, Taxi-v3. Default: CliffWalking-v0'
+                   )
+
+
+parser.add_argument('-g', '--greedy',
+                    type=lambda s: s.lower() in ['true', 't', 'yes', '1'],
+                    default=True,
+                    help='Choose greedy action only. Default: True'
+                   )
+
+
+
+parser.add_argument('-a', '--agent',
+                    type=str,
+                    default='SARSA',
+                    choices = ['QLearning', 'SARSA'],
+                    help='Agent. One of QLearning, SARSA. Default: QLearning'
+                   )
+
+
+parser.add_argument('-f', '--filename',
+                    type=str,
+                    default=None,
+                    help='Filename to load Q_est from. Default: None (it is choosen by the Agent then)'
+                   )
+
+
+
+
+def render_env(agent, env, epsilon=0.1, greedy=False):
+    path = []
+    state, info = env.reset()
+    states = [state]
+    total_reward = 0
+    total_steps = 0
+    done = False
+    env.render()
+    while not done:
+        if greedy:
+            action = agent.Q_est[state].argmax()
+        else:
+            action = agent.get_action(state)
+            
+        next_state, reward, done, truncated, info = env.step(action)
+        total_reward += reward
+        state = next_state
+        total_steps += 1
+        states.append(state)
+        env.render()
+    return states, total_reward, total_steps
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    env = gym.make(args.env, render_mode = 'human')
+    
+    if args.agent == 'QLearning':
+        agent  = QLearningAgent(env, alpha=0.1, gamma=0.95, epsilon=0.1)
+    else:
+        agent = SarsaAgent(env, alpha=0.1, gamma=0.95, epsilon=0.1)
+
+    if args.filename is None:
+        agent.load()
+    else:
+        agent.load(args.filename)
+    states, total_reward, total_steps = render_env(agent, env, agent.epsilon, greedy=args.greedy)
+    print (f'Reward {total_reward} achieved in {total_steps} steps.')
+    print ('States: {}'.format(states))
+    
+    
